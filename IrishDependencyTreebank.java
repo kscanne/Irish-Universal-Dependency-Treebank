0a05c87f1c55cd3138f2d70c00ef6a371f8c0e6f
tlynn747 <teresa.m.lynn@gmail.com>
2015-01-05 16:04:45 +0000

    ROOT -> root; and fixed loops
    Couple of loops identified after running validate.py - now fixed.
    Also changed ROOT to root

--- IrishDependencyTreebank.java
+++ IrishDependencyTreebank.java
@@ -18,7 +18,11 @@ import java.util.ArrayList;
  * @author fosterjen
  */
 public class IrishDependencyTreebank {
+<<<<<<< Updated upstream
     static ArrayList<Sentence> sentences = new ArrayList();
+=======
+    static ArrayList sentences = new ArrayList();
+>>>>>>> Stashed changes
     /**
      * @param args the command line arguments
      */
@@ -29,6 +33,7 @@ public class IrishDependencyTreebank {
             int lineCount = 0;
             BufferedReader b = new BufferedReader(new FileReader(new File(args[0])));
             String line = b.readLine();
+<<<<<<< Updated upstream
             ArrayList<String> lines;
             while (line != null)
             {
@@ -49,6 +54,18 @@ public class IrishDependencyTreebank {
             //processProPreps();
 	    processPPXComp();
             printSentences();
+=======
+            
+            while (line != null)
+            {
+                sentences.add(line);
+                line = b.readLine();         
+            }      
+            b.close();
+             //processPP(sentences);
+             processCopula(sentences);
+             printSentences(sentences);
+>>>>>>> Stashed changes
         }
         catch (IOException io)
         {
@@ -57,7 +74,11 @@ public class IrishDependencyTreebank {
        
     }
     
+<<<<<<< Updated upstream
     public static void processPP() 
+=======
+    public static void processPP(ArrayList sentences) 
+>>>>>>> Stashed changes
     {
         Sentence sentence;
                   
@@ -280,6 +301,7 @@ public class IrishDependencyTreebank {
                 Word caseWord = caseWords.get(j);
                 if (caseWord != null && caseWord.getToken().startsWith("***"))
                 {
+<<<<<<< Updated upstream
                     if (isProPrep(caseWord.getToken().substring(3,caseWord.getToken().length()-3),caseWord.getLemma()))
                     {
                          caseWord.setToken(caseWord.getToken().substring(3,caseWord.getToken().length()-3));
@@ -314,6 +336,145 @@ public class IrishDependencyTreebank {
             }
         }
         return false; //To change body of generated methods, choose Tools | Templates.
+=======
+                    sentences.set(prepLine,"***"+line+"***");
+                }
+            }
+        }
+       
+    }
+    
+    public static void processCopula(ArrayList sentences) 
+    {
+        int lineCount = 0, copLine = 0, predLine = 0;
+        String line;
+       
+        for (int i=0; i < sentences.size(); i++)
+        {
+            line = (String) sentences.get(i); 
+            boolean isNotAPred = false;
+            boolean isNotASubj = false;
+            boolean finishedSentence = false;
+            boolean finishedFile = false;
+            //System.out.println("LINE" + line);
+            lineCount++;
+            String nextLine;
+            String [] tokens = line.split("\t");    
+            if (tokens.length > 1 && tokens[2].equals("is") &&!tokens[7].equals("mark:prt"))
+            {
+                String copId = tokens[0];
+                String copHeadId = tokens[6];
+                copLine = i;
+                lineCount = i;
+                
+                String [] predTokens = null;
+                String predHead = null;
+                String predId = null;
+                String [] subjTokens = null;
+                String subjHead = null;
+                
+                do
+                {
+                    lineCount++;
+                    isNotAPred = false;
+                    nextLine = (String) sentences.get(lineCount);
+                    //System.out.println("NEXTLINE " + nextLine);
+                        
+                    if (nextLine == null)
+                    {
+                        finishedFile = true;
+                    }
+                    else if (nextLine.length() <= 1)
+                    {
+                        finishedSentence = true;     
+                    }
+                    else 
+                    {
+                        predTokens = nextLine.split("\t");
+                        predId = predTokens[0];
+                        predHead = predTokens[6];
+                        if (!predTokens[7].startsWith("xcomp") || !predHead.equals(copId))
+                        {
+                            //System.out.println("not a pred");
+                            isNotAPred = true; 
+                        }    
+                    }             
+                }while (isNotAPred && !finishedSentence && !finishedFile);
+                   
+                if (!finishedSentence && !finishedFile)
+                {
+                    //the new head of the pred is the head of the cop
+                    predTokens[6] = copHeadId;
+                    //the relation associated with the cop is now associated with the pred
+                    predTokens[7] = tokens[7];
+                    //the new head of the cop is the pred
+                    tokens[6] = predId;
+                    //the cop's relation to the pred is "cop"
+                    tokens[7] = "cop";
+                    predLine = lineCount;
+                    //now find the subj of the predicate and anything else that used to depend on the copula verb
+                    String anotherLine = null;
+                    do
+                    {
+                        lineCount++;
+                        anotherLine = (String) sentences.get(lineCount);
+                        //System.out.println("NEXTLINE " + anotherLine);
+                        
+                        if (anotherLine == null)
+                        {
+                            finishedFile = true;
+                        }
+                        else if (anotherLine.length() <= 1)
+                        {
+                            finishedSentence = true;     
+                        }
+                        else 
+                        {
+                            subjTokens = anotherLine.split("\t");
+                            subjHead = subjTokens[6];
+                            if (subjHead.equals(copId))
+                            {
+                                subjTokens[6] = predId;
+                                anotherLine = "";
+                                for (int x=0; x < subjTokens.length-1; x++)
+                                {
+                                    anotherLine += subjTokens[x] + "\t";
+                                }
+                                anotherLine += subjTokens[subjTokens.length-1];
+                                sentences.set(lineCount,anotherLine);
+                            }    
+                        }             
+                    }while (!finishedSentence && !finishedFile);
+                    
+                    line = ""; nextLine = "";
+                    for (int k=0; k < tokens.length-1; k++)
+                    {
+                        line += tokens[k] + "\t";
+                    }
+                    line += tokens[tokens.length-1];
+                    for (int j=0; j < predTokens.length-1; j++)
+                    {
+                        nextLine += predTokens[j] + "\t";
+                    }
+                    nextLine += predTokens[predTokens.length-1];
+                    sentences.set(copLine,line);
+                    sentences.set(predLine,nextLine);
+                }
+                else if (finishedSentence || finishedFile)
+                {
+                    sentences.set(copLine,"+++"+line+"+++");
+                }
+            }
+        }
+       
+    }
+    public static void printSentences(ArrayList sentences)
+    {
+         for (int i=0; i < sentences.size(); i++)
+        {
+            System.out.println(sentences.get(i));
+        }      
+>>>>>>> Stashed changes
     }
 
     
